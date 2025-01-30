54d017f37cc45b12dae0495709ea5c71a7ba8285
Aditya Grover <axg1595@mavs.uta.edu>
2025-01-29 19:09:31 -0600

    Adding initial Account.java

---
+++ P03/full_credit/Account.java
@@ -0,0 +1,21 @@
+public class Account{
+
+private String name;
+private int id;
+private static int nextID;
+private AccountStatus status;
+
+public Account(String name){
+   if(name.length()==0) {
+    throw new IllegalArgumentException("Name cant be empty");
+   }
+   this.name=name;
+   this.id=nextID++;
+   this.status=AccountStatus.Normal;
+}
+
+public void setStatus(AccountStatus status){
+    this.status=status;
+}
+
+}
