31c26250e60d9a40c4bcda14479b19827b637204
Aditya Grover <axg1595@mavs.uta.edu>
2025-02-02 01:20:15 -0600

    adding more sender names

--- P03/bonus1/DemoMessage.java
+++ P03/bonus1/DemoMessage.java
@@ -8,27 +8,34 @@ public class DemoMessage {
         ArrayList<Account> accounts = new ArrayList<>();
         accounts.add(new Account("jam"));
         accounts.add(new Account("sam"));
+        accounts.add(new Account("bethany"));
+        accounts.add(new Account("george"));
         
         System.out.print("Enter message body: ");
         String body = scanner.nextLine();
         
-        System.out.print("Enter sender name (available senders: sam and jam): ");
+        System.out.print("Enter sender name (available senders: sam, jam, bethany, george): ");
         String senderName = scanner.nextLine().trim();
         
         Account sender = null;
+        
         if (senderName.equals("jam")) {
             sender = accounts.get(0);
         } else if (senderName.equals("sam")) {
             sender = accounts.get(1);
+        } else if (senderName.equals("bethany")) {
+            sender = accounts.get(2);
+        } else if (senderName.equals("george")) {
+            sender = accounts.get(3);
+        } else {
+            System.out.println("sender name is invalid");
+            scanner.close();
+            return;
         }
         
-        if (sender != null) {
-            Message message = new Message(sender, null, body);
-            System.out.println("\nMessage Created:");
-            System.out.println(message);
-        } else {
-            System.out.println("Invalid sender name!");
-        }
+        Message message = new Message(sender, null, body);
+        System.out.println("\nMessage Created:");
+        System.out.println(message);
         
         scanner.close();
     }
