114adbba190b8c794d01822a0c107de41a5b0b83
Aditya Grover <axg1595@mavs.uta.edu>
2025-02-06 22:36:58 -0600

    initial DirectMessage.java

---
+++ P04/message/DirectMessage.java
@@ -0,0 +1,14 @@
+package message;
+import account.Account;
+public class DirectMessage extends Message {
+    private Account to;
+
+    public DirectMessage(Account from, Account to, Message repliedTo, String body){
+        super(from,repliedTo,body);
+        this.to=to;
+    }
+    
+//     public String toString(){
+        
+//     }
+ }
