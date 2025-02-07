3f4ef3f980b4b06c144ac9a98a714f70b73a9a76
Aditya Grover <axg1595@mavs.uta.edu>
2025-02-07 00:58:19 -0600

    added toString to direct message

--- P04/message/DirectMessage.java
+++ P04/message/DirectMessage.java
@@ -1,14 +1,16 @@
 package message;
+
 import account.Account;
+
 public class DirectMessage extends Message {
     private Account to;
 
-    public DirectMessage(Account from, Account to, Message repliedTo, String body){
-        super(from,repliedTo,body);
-        this.to=to;
+    public DirectMessage(Account from, Account to, Message repliedTo, String body) {
+        super(from, repliedTo, body);
+        this.to = to;
     }
-    
-//     public String toString(){
-        
-//     }
- }
+
+    public String toString() {
+        return "To: " + to + " " + super.toString();
+    }
+}
