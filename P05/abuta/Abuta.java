6d21f45dbca44054c3916476ce5fae999f3439b6
Aditya Grover <axg1595@mavs.uta.edu>
2025-02-24 00:50:02 -0600

    finalized abuta.java(made minor changes)

--- P05/abuta/Abuta.java
+++ P05/abuta/Abuta.java
@@ -1,7 +1,6 @@
 package abuta;
+
 import java.util.List;
-
-
 
 import java.util.ArrayList;
 import menu.Menu;
@@ -12,8 +11,6 @@ import message.Group;
 import message.Message;
 import message.Post;
 
-
-
 public class Abuta {
     private List<Account> accounts;
     private List<Group> groups;
@@ -22,10 +19,10 @@ public class Abuta {
     private String output;
     private boolean running;
 
-    public Abuta(){
-        accounts=new ArrayList<>();
-        groups=new ArrayList<>();
-
+    public Abuta() {
+        accounts = new ArrayList<>();
+        groups = new ArrayList<>();
+        output = "";
         accounts.add(new Account("Aditya"));
         accounts.add(new Account("Fred"));
         accounts.add(new Account("George"));
@@ -37,9 +34,9 @@ public class Abuta {
         groups.add(new Group("Mcdonalds Rocks"));
         groups.add(new Group("Gym"));
         groups.add(new Group("Chipotle Lovers"));
-    
-        message = new Post(accounts.get(0), groups.get(0),null,"Welcome to Microwave Fans.");  
-        
+
+        message = new Post(accounts.get(0), groups.get(0), null, "Welcome to Microwave Fans.");
+
         menu = new Menu();
         menu.addMenuItem(new MenuItem("Exit", () -> endApp()));
         menu.addMenuItem(new MenuItem("Show Reply", () -> showReply()));
@@ -47,83 +44,74 @@ public class Abuta {
         menu.addMenuItem(new MenuItem("Add Reply", () -> reply()));
     }
 
-    public void mdi(){
-        running=true;
-        while(running){
-            try{
-                System.out.println("abUTA You");
+    public void mdi() {
+        running = true;
+        while (running) {
+            try {
+                System.out.println("\n            - = #abUTA You- = #\n");
                 System.out.println(menu);
                 System.out.println(message);
                 System.out.println(output);
                 output = "";
-                int choice = Menu.getInt("Choice? ");
+                int choice = Menu.getInt("Selection: ");
                 menu.run(choice);
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("e.getMessage()");
             }
         }
     }
 
-    private void endApp(){
-        running=false;
+    private void endApp() {
+        running = false;
     }
 
-    private void showRepliedTo(){
-        if(message.getRepliedTo()==null){
-            output="no message was replied to ";
-        }
-        else
-        message=message.getRepliedTo();
+    private void showRepliedTo() {
+        if (message.getRepliedTo() == null) {
+            output = "no message was replied to ";
+        } else
+            message = message.getRepliedTo();
     }
-    
-    private void showReply(){
-        if(message.getNumReplies()==0){
-            output="no replies to show";
-        }
-        else if(message.getNumReplies()==1){
-            message=message.getReply(0);
-        }
-        else{
-            int index = Menu.getInt("Which reply (0-" + (message.getNumReplies()-1) + ")? ");
-            if(index >= 0 && index < message.getNumReplies()) {
+
+    private void showReply() {
+        if (message.getNumReplies() == 0) {
+            output = "no replies to show";
+        } else if (message.getNumReplies() == 1) {
+            message = message.getReply(0);
+        } else {
+            int index = Menu.getInt("Which reply (0-" + (message.getNumReplies() - 1) + ")? ");
+            if (index >= 0 && index < message.getNumReplies()) {
                 message = message.getReply(index);
             } else {
                 output = "Invalid reply number";
             }
         }
 
-        }
+    }
 
-        private void reply(){
-             try {
-            char type = Menu.getChar("Post or Direct Message (p/d)? ");
-            
-            int authorIndex = Menu.selectItemFromList("Select author", accounts);
+    private void reply() {
+        try {
+            char type = Menu.getChar("Post or Direct Message (p/d): ");
+
+            int authorIndex = Menu.selectItemFromList("Select author: ", accounts);
             Account author = accounts.get(authorIndex);
-            
-            if(type == 'p' || type == 'P') {
-                int groupIndex = Menu.selectItemFromList("Select group", groups);
+
+            if (type == 'p' || type == 'P') {
+                int groupIndex = Menu.selectItemFromList("Select group: ", groups);
                 Group group = groups.get(groupIndex);
                 String text = Menu.getString("Enter post text: ");
                 message = new Post(author, group, message, text);
-            } else if(type == 'd' || type == 'D') {
-                int recipientIndex = Menu.selectItemFromList("Select recipient", accounts);
+            } else if (type == 'd' || type == 'D') {
+                int recipientIndex = Menu.selectItemFromList("Select recipient: ", accounts);
                 Account recipient = accounts.get(recipientIndex);
                 String text = Menu.getString("Enter message text: ");
                 message = new DirectMessage(author, recipient, message, text);
             }
-        } catch(Exception e) {
-            output = "Error creating reply: " + e.getMessage();
-        }
+        } catch (Exception e) {
+            output = e.getMessage();
         }
     }
-    
-
-
-
-
-
-
-
-    
+    public static void main(String[] args) {
+        Abuta app = new Abuta();
+        app.mdi();
+    }
+}
